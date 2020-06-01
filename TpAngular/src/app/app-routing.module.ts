e65073f23c0c78abf688157502c91123eca1994d
Georgina <georgi888111@gmail.com>
2020-06-01 20:22:57 -0300

    agrego home, ubicacion y navBar

--- TpAngular/src/app/app-routing.module.ts
+++ TpAngular/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {UbicacionComponent} from './components/ubicacion/ubicacion.component'
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+    { path: 'home', component: HomeComponent }, 
+    { path: 'ubicacion', component: UbicacionComponent },
+    //{ path: 'productos', component: ProductoComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' } 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- TpAngular/src/app/app.module.ts
+++ TpAngular/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {HomeComponent} from './components/home/home.component';
+import {UbicacionComponent} from './components/ubicacion/ubicacion.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UbicacionComponent,
+    NavbarComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
